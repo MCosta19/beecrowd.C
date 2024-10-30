f695a4de009daf35b90c444385bb4fd2bb0a7d19
Matheus Costa <matheusca2006@gmail.com>
2024-10-29 22:13:35 -0300

    inicio da lista 2

---
+++ C_mais_ou_menos.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    
+    int lop, vtm, i = 0, qnt, total = 0, dif;
+    char fruit[20];
+
+    while (i < lop) {
+        scanf("%d", &lop);
+        if (lop == 0) {
+            break;
+        }
+
+        scanf("%d %s", &qnt, fruit);
+
+        if (strcmp(fruit, "suco de laranja") == 0) {
+            vtm = qnt * 120;
+                    
+            total = total + vtm;
+        }
+        else if (strcmp(fruit, "morango fresco") == 0 || strcmp(fruit, "mamao") == 0) {
+            vtm = qnt * 85;
+
+            total = total + vtm;
+        }
+        else if (strcmp(fruit, "goiaba vermelha") == 0) {
+            vtm = qnt * 70;
+
+            total = total + vtm;
+        }
+        else if (strcmp(fruit, "manga") == 0) {
+            vtm = qnt * 56;
+
+            total = total + vtm;
+        }
+        else if (strcmp(fruit, "laranja") == 0) {
+            vtm = qnt * 50;
+
+            total = total + vtm;
+        }
+        else if (strcmp(fruit, "brocolis") == 0) {
+            vtm = qnt * 34;
+
+            total = total + vtm;
+        }
+
+        i++;
+   }
+
+    if (total < 110) {
+        dif = 110 - total;
+
+        printf("Mais %d mg", dif);
+    }
+    else if (total >= 110 && total <= 130) {
+        printf("%d mg", total);
+
+    }
+    else if (total > 130) {
+        dif = total - 130;
+
+        printf("Menos %d mg", dif);
+    }
+
+    return 0;
+}
